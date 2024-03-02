7e7d4ea2bf09cd7dafda82b2d3a52b3f3c942859
seafood12 <abieledelso@gmail.com>
2024-03-02 22:29:07 +0000

    Add AddDocumentProvider to MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_docs/providers/add_document_provider.dart';
 import 'package:my_docs/providers/search_provider.dart';
 import 'package:my_docs/router/app_router.dart';
 import 'package:my_docs/themes/app_theme.dart';
@@ -13,7 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => SearchProvider())
+        ChangeNotifierProvider(create: (context) => SearchProvider()) ,
+        ChangeNotifierProvider(create: (context) => AddDocumentProvider())
       ],
       child: MaterialApp.router(
         theme: AppTheme(selectedColor: 3).theme(),
